e3e22e31a6494567a14d22963f438c7619fb4b1e
AndrewRomanyshyn <morisszalivaet228@mail.ru>
2022-08-09 11:49:06 +0200

    First commit

---
+++ BasePython/lesson2.py
@@ -0,0 +1,35 @@
+# # a = float(input("Введіть перше число:"))
+# # b = float(input("Введіть друге число:"))
+# # c = float(input("Введіть третє число:"))
+# #
+# # print(a + b + c)
+#
+#
+# age = 25
+#
+# print(type(age))
+
+# int
+# float
+# boolean
+# str
+
+# print(1 + 1)
+# print(1 + 1.1)
+# print(1 + True)
+# print(str(1) + "stroka")
+
+# print(1.1 + 1)
+# print(1.1 + 1.1)
+# print(1.1 + False)
+# print(str(1.1) + "andrew")
+
+# print(True + 1)
+# print(True + 1.1)
+# print(True + False)
+# print(str(True) + "Andrew")
+
+print("andrew" + str(1))
+print("andrew" + str(1.1))
+print("andrew" + str(False))
+print("andrew" + "stroka")
---
+++ BasePython/lesson3.py
@@ -0,0 +1,49 @@
+# Арифметичні операції в пайтоні
+# +  - додавання
+# -  - віднімання
+# *  - множення
+# /  - ділення
+# // - цілочислене ділення
+# ** - піднесення до степеня
+# %  - залишок від ділення
+
+# print(6 + 2)
+# print(6 - 2)
+# print(6 * 2)
+# print(6 / 2)
+
+# print(7 / 2)
+# print(7 // 2)
+# print(9 // 2)
+
+# print(6 ** 2)
+# print(pow(6, 2))
+
+# print(7 % 2)
+
+
+# Арифметичні операції з присвоєнням
+# +=
+# -=
+# *=
+# /=
+# //=
+# **=
+# %=
+
+
+# number = 10
+# number += 5
+# print(number)
+
+
+# firstNumber = float(input("введіть перше значення: "))
+# secondNumber = float(input("Введіть друге значення: "))
+# print(firstNumber / secondNumber)
+# print(round(firstNumber / secondNumber, 6))
+
+
+# print(round(2.49))
+# print(round(2.5))
+# print(round(2.50001))
+
