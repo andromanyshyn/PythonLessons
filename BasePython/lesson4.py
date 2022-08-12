56da9a247005a3b1966bdd30987f5e8cd9d653f5
AndrewRomanyshyn <morisszalivaet228@mail.ru>
2022-08-12 11:54:47 +0200

    Conditional expressions

---
+++ BasePython/lesson4.py
@@ -0,0 +1,32 @@
+# Умовні вирази
+
+# Операції порівняння
+
+# a = float(input("введіть число a : "))
+# b = float(input("введіть число b : "))
+#
+# print(a == b)
+# print(a != b)
+# print(a > b)
+# print(a < b)
+# print(a >= b)
+# print(a <= b)
+
+
+# Логічні операції
+
+# a = float(input("введіть число a : "))
+# b = float(input("введіть число b : "))
+#
+# print(a == 1 and b == 1)
+# print(a == 1 or b == 1)
+# print(not a > b)
+
+
+# операція in (в)
+
+# users = ["Serhii", "Andrew", "Oleg"]
+# user = "Serhii"
+#
+# print(user in users)
+
