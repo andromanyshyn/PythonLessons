f35b531fbd633f459b805c709eabc91693862ea1
AndrewRomanyshyn <morisszalivaet228@mail.ru>
2022-08-30 11:38:39 +0200

    Learning list methods

---
+++ BasePython/lesson8.py
@@ -0,0 +1,32 @@
+# lst = [1, 1, 2, 6, 5, 12, 15, 20]
+
+
+# append(item): добавляємо елемент в кінець списку
+# lst.append("python")
+
+# insert(index, item):
+# lst.insert(0, "name")
+
+# extend(items):
+# lst.extend([1, 15, 3])
+
+# remove(item):
+# lst.remove(1)
+
+# clear():
+# lst.clear()
+
+# index(item):
+# lst.index(20)
+
+# pop([index]):
+# lst.pop(0)
+
+# count(item):
+# lst.count(1)
+
+# sort([key])
+# lst.sort()
+
+
+
