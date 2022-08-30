6367d52d5723bd312d902e00b53ade63c0374ac7
AndrewRomanyshyn <morisszalivaet228@mail.ru>
2022-08-30 11:38:17 +0200

    Learning list methods

---
+++ Theory/Список(list).py
@@ -0,0 +1,33 @@
+
+# print(numbers)
+# print(type(numbers))
+# print("----------------")
+
+# print(sum(lst)
+# print("len -", len(numbers), "кількість елементів!")
+# print("max -", max(numbers), "максимальне значення в лісті")
+# print("min -", min(numbers), "мінімальне значення в лісті")
+# print("sorted", sorted(numbers), "відсортований ліст елементів по зростанню")
+# print("sorted", sorted(numbers, reverse=True), "відсортований ліст елементів по спаданню")
+# print("sorted просто виводить відсортований ліст але:", numbers)
+
+
+# list("python")
+# print(list("python"))
+# numbers = numbers + numbers
+# sum(lst)
+
+# Методи :
+
+# 1) .index() - знаходить індекс вписаного елемента
+# 2) .count() - рахує скільки раз значення було в списку
+# 3) .append() - добавляє значення в список
+# 4) .sort() - сортує список по зростанню
+# 5) lst.sort(reverse = True) сортую список по спаданню
+# 6) lst.remove(1) - перше яке попаде значення удаляє
+# 7) .pop(index) - удаляєм елемент по індексу
+# 8) .extend() - можемо добавляти більше елементів (список) lst.extend([1, 2, 3])
+# 8) ", ".join(lst) - перетворюємо список в строку
+# 9) .insert(3, [1, 2]) - вставляє елементи перед конкретним індексом
+# 10) numbers = [i for i in range(1,6)]
+# # [1, 2, 3, 4, 5]
