3e4a883879a93a61ef24542898595ab5173ed7fb
AndrewRomanyshyn <morisszalivaet228@mail.ru>
2022-09-04 10:22:21 +0200

    Learning Tuple

---
+++ BasePython/lesson10.py
@@ -0,0 +1,27 @@
+# t = (1, 3, 4)
+# print(t)
+
+# t = (1, 3, 4, 5, 6)
+#
+# count = 0
+#
+# while count < len(t):
+#     print(t[count])
+#     count += 1
+
+
+# users = ("Andrew", "Serhii", "Oleg", "Ira", "Vitalik")
+#
+# name = input("Введіть ім'я: ")
+# if name in users:
+#     print(True)
+# else:
+#     print(False)
+
+
+# t = (1, 3, 4, 5, 6)
+#
+# print(t[1:4])
+
+# a = (1, 2, 3, 4, 5, 6, 7)
+# print(a[::2])
